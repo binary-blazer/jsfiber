733b9db93751022f19bd982d90119ae6e8a66ba4
Jonas Franke <jonasfranke@sdevs.org>
2025-03-08 15:59:11 +0100

    Update middleware system to support private and public functions

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,17 +8,6 @@ class Middleware {
   public use(middleware: Function): void {
     this.middlewares.push(middleware);
   }
-
-  public executeMiddlewares(req: any, res: any, next: Function): void {
-    const execute = (index: number) => {
-      if (index < this.middlewares.length) {
-        this.middlewares[index](req, res, () => execute(index + 1));
-      } else {
-        next();
-      }
-    };
-    execute(0);
-  }
 }
 
 const middleware = new Middleware();
